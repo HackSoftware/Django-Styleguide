f98cbebe2b616e038e846a3811bcdfb9aa5a3acc
Radoslav Georgiev <radorado@hacksoft.io>
2018-07-26 12:40:05 +0300

    Add `utils.py` with `inline_serializer` implementation

---
+++ utils.py
@@ -0,0 +1,11 @@
+def create_serializer_class(name, fields):
+    return type(name, (serializers.Serializer, ), fields)
+
+
+def inline_serializer(*, fields, data=None, **kwargs):
+    serializer_class = create_serializer_class(name='', fields=fields)
+
+    if data is not None:
+        return serializer_class(data=data, **kwargs)
+
+    return serializer_class(**kwargs)
